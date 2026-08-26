af966b8fef0fe23b1caad76b84804736c69e34a4
Mohamed Al Hafees <malhafees@gmail.com>
2026-08-26 11:56:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let studentName = "Mohamed";
+let studentAge = 25;
+let studentMark = 85;
+
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Student Mark:", studentMark);
+
+let userName = prompt("Enter your name:");
+
+alert("Hello " + userName);
+
+let userAge = prompt("Enter your age:");
+
+console.log("User Age:", userAge);
+
+let answer = confirm("Do you like JavaScript?");
+
+console.log("User Answer:", answer);
+
+let name = prompt("Enter your name again:");
+document.writeln("<h2>Hello " + name + "</h2>");
